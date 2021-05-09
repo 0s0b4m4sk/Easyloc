7d2f81e221168ed6c3e4a6705f7edb24634e0261
Redadjzz <83847443+Redadjzz@users.noreply.github.com>
2021-05-09 04:50:22 +0200

    Update Index.php
    ajout bdd

--- E4/Index.php
+++ E4/Index.php
@@ -62,7 +62,39 @@
               </div>
             </nav>
 </head>
-<?php ?>
+<?php 
+
+  //Exemple de syntaxe de connexion à la base de données pour PHP et MySQL.
+  
+  //Se connecter à la base de données
+
+            $servername = 'localhost';
+            $username = 'root';
+            $password = 'root';
+            
+            //On établit la connexion
+            $conn = new mysqli($servername, $username, $password);
+            
+            //On vérifie la connexion
+            if($conn->connect_error){
+                die('Erreur : ' .$conn->connect_error);
+            }
+            echo 'Connexion réussie';
+
+             //On essaie de se connecter
+            try{
+                $conn = new PDO("mysql:host=$servername;dbname=bddtest", $username, $password);
+                //On définit le mode d'erreur de PDO sur Exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo 'Connexion réussie';
+            }
+            
+            /*On capture les exceptions si une exception est lancée et on affiche
+             *les informations relatives à celle-ci*/
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+        ?>
 
 <body>          
 
